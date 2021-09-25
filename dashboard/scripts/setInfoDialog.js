6cd42405ff7ff722caff70f46273560774901af5
Dylan Fernald <60404590+drf4804@users.noreply.github.com>
2021-09-24 20:35:52 -0400

    Adding Renders & Icons for DLC Up to Kazuya (#10)
    * adding stock icons for 3 dlc characters
    
    * adding renders for 3 dlc characters
    
    * updated to include dlc up to Kazuya

--- dashboard/scripts/setInfoDialog.js
+++ dashboard/scripts/setInfoDialog.js
@@ -159,7 +159,7 @@ function getCharacters(game){
 	var ultimate = wiiu.slice(0, -1).concat(["IceClimbers", "YoungLink", "Snake", "Wolf", "PokemonTrainerMale", "PokemonTrainerFemale", "Squirtle",
 							"Ivysaur", "Daisy", "PiranhaPlant", "KingK.Rool", "Pichu", "Ridley", "DarkSamus", "Incineroar",
 							"Chrom", "Isabelle", "InklingBoy", "InklingGirl", "Ken", "Simon", "Richter", "Joker", "MiiGunner",
-							"MiiSwordfighter", "MiiBrawler", "Banjo&Kazooie", "Hero", "Terry", "BylethMale", "BylethFemale", "MinMin", "Steve"]); // Extendable for future DLC
+							"MiiSwordfighter", "MiiBrawler", "Banjo&Kazooie", "Hero", "Terry", "BylethMale", "BylethFemale", "MinMin", "Steve", "Sephiroth", "PyraMythra", "Kazuya"]); // Extendable for future DLC
 	switch(game){
 		// Sort the lists so they look decent in the dropdown.
 		case "ssb64":
